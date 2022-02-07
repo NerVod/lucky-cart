149f5bb698a86e10d5a19ccad131016a7610df5c
NerVod <benjamin.jeannerot@gmail.com>
2022-02-07 22:11:25 +0100

    all conditions checked

--- src/eligibility.service.js
+++ src/eligibility.service.js
@@ -47,6 +47,21 @@ class EligibilityService {
       }
     }
 
+
+     // test condition 1 shopper id
+     let shopperId
+     if(cartTest.shopperId === criteriaTest.shopperId ) {
+       shopperId = true
+     } else {
+       shopperId = false;
+     } 
+     console.log(cartTest.shopperId)
+     console.log(criteriaTest.shopperId)
+     console.log(shopperId)
+   
+
+
+      // test condition 2 : total articles
     let totalAti; 
     if(cartTest.totalAti >= criteriaTest.totalAti.gt ) {
       totalAti = true;
@@ -59,37 +74,35 @@ class EligibilityService {
 
 
 
-    
-    let shopperId
-    if(cartTest.shopperId === criteriaTest.shopperId ) {
-      shopperId = true
-    } else {
-      shopperId = false;
-    } 
-    console.log(cartTest.shopperId)
-    console.log(criteriaTest.shopperId)
-    console.log(shopperId)
-  
-
-
-
-    let productId
+    // test condition 3 product id
+    let productId = false;
     for(let i=0; i<cartTest.products.length; i++){
       console.log('cartest product i :',cartTest.products[i].productId)
       console.log('criteria product id :', criteriaTest["products.productId"].in.toString())
       if(criteriaTest["products.productId"].in.toString() === cartTest.products[i].productId) {
-        return productId = true;
-      } else {
-        productId = false
+       productId = true;
       }
     }
-    console.log(productId)
+    console.log('product id :',productId)
 
 
+    // test condition 4 dates valides
+    let date = false;
+    console.log('datecriteria début:',criteriaTest.date.and.gt )
+    console.log('date carteTest :',cartTest.date )
+    console.log('datecriteria fin:',criteriaTest.date.and.lt )
+    if(criteriaTest.date.and.gt<cartTest.date &&  cartTest.date<criteriaTest.date.and.lt){
+       date = true
+    } 
+    console.log('date : ', date)
 
 
-    
-    return false;
+     // vérification de toutes les conditions d'éligibilité 
+    if(totalAti && shopperId && productId && date ){
+      return true
+    }else {
+      return false;
+    }
   }
 }
 
